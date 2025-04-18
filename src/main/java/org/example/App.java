f4afb496ef13d686f219ae5307173184e78b340f
hubstudent65 <167081196+hubstudent65@users.noreply.github.com>
2025-04-18 14:55:46 +0300

    git commit -m "App.java"

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,7 +7,8 @@ package org.example;
 public class App 
 {
     public static void main( String[] args )
+
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Git Hub !" );
     }
 }
